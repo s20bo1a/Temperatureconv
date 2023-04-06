629a081cfe124d2bf7449bef8521a143735e1639
sindhu <botlasindhu@gmail.com>
2023-04-06 17:58:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const calculateTemp=()=>{
+    const inputTemp=document.getElementById('temp').value;
+     const tempSelected=document.getElementById('temp_diff')
+     const valueTemp=temp_diff.options[tempSelected.selectedIndex].value;
+      const celToFah=(cel)=>{
+        let fahrenheit=((cel*9/5)+32).toFixed(1);
+        return fahrenheit;
+      }
+      const fahtocel=(fah)=>{
+        let celsius=((fah-32)*5/9).toFixed(1);
+        return celsius;
+      }
+      if(valueTemp=='cel'){
+        document.getElementById("result").innerHTML=celToFah(inputTemp)+"&#176;F";
+      }
+      else{
+        document.getElementById("result").innerHTML=fahtocel(inputTemp)+"&#176;C"
+      }
+
+}
